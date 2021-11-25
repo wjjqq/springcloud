1397f43ad955367a79fdd9a9cfd22e85c184945f
wang.jinjin <wang.jinjin@rongzer.com>
2021-11-25 09:02:40 +0800

    springcloud集成sentinel nacos集群

---
+++ springcloud-04-nacos-cluser/src/test/java/com/wjj/nacos/Springcloud04NacosCluserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjj.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud04NacosCluserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

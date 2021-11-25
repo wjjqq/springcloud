1cc6d27d57e76b581630d6ca0649b823750c9c17
wang.jinjin <wang.jinjin@rongzer.com>
2021-11-25 09:03:44 +0800

    springcloud集成zookeeper补充提交

---
+++ springcloud-01-zookeeper-provider2/src/main/java/com/wjj/ribbon/Springcloud01ZookeeperProvider2Application.java
@@ -0,0 +1,13 @@
+package com.wjj.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud01ZookeeperProvider2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud01ZookeeperProvider2Application.class, args);
+    }
+
+}
---
+++ springcloud-01-zookeeper-provider2/src/test/java/com/wjj/ribbon/Springcloud01ZookeeperProvider2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjj.ribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springcloud01ZookeeperProvider2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

40d71368de1d3efa18af9a8e50378ea2c3874f1e
wang.jinjin <wang.jinjin@rongzer.com>
2021-11-30 16:06:11 +0800

    springboot-db

---
+++ springboot-02-db/src/main/java/com/wjj/db/Springboot02DbApplication.java
@@ -0,0 +1,13 @@
+package com.wjj.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02DbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02DbApplication.class, args);
+    }
+
+}
---
+++ springboot-02-db/src/test/java/com/wjj/db/Springboot02DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjj.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
